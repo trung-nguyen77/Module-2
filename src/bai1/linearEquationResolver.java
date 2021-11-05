d0226234c8aa8b0fcc46c0c276d1a8127b71793e
trung-nguyen77 <nguyentrung0866707088@gmail.com>
2021-11-05 11:16:33 +0700

    is Prime

---
+++ src/bai1/linearEquationResolver.java
@@ -0,0 +1,10 @@
+package bai1;
+
+import java.util.Arrays;
+
+public class linearEquationResolver {
+    public static void main(String[] args) {
+        System.out.println("Linear Equation Resolver");
+        System.out.println("Given a equation as 'a * x + b = c', please enter constants:");
+    }
+}
--- src/bai2/SNT.java
+++ src/bai2/SNT.java
@@ -1,4 +1,22 @@
 package bai2;
 
 public class SNT {
+    public static void main(String[] args) {
+        int x = 0;
+        int count = 0;
+        for (int i = 13;count < 20 ;i++) {
+            boolean check = true;
+            for (int j = 2;j <= Math.sqrt(x); j++) {
+                if (i % j == 0) {
+                    check = false;
+                    break;
+                }
+            }
+            if (check) {
+                count++;
+                x = i;
+            }
+        }
+        System.out.println(x);
+    }
 }
