cb9d9cbffef1fa567db212d3678cbdc501be5bfc
trung-nguyen77 <nguyentrung0866707088@gmail.com>
2021-11-18 14:24:07 +0700

    stack_LinkedList

--- src/bai10/ArrayListExercise/MyList.java
+++ src/bai10/ArrayListExercise/MyList.java
@@ -9,18 +9,30 @@ public class MyList<E> {
     public MyList() {
     }
 
-    public MyList(int capacity) {
+    public MyList(int capacity) {;
 
     }
     public void add(int index, E element){
-        Object[] arr = new Object[elements.length + 1];
-        for (int i = 0; i < ; i++) {
-            
+        Object[] arrList = new Object[elements.length + 1];
+        for (int i = 0; i < elements.length; i++) {
+            arrList[i] = elements[i];
         }
+        arrList[arrList.length -1] = elements;
     }
     
     public E remove(int index){
-        
+        Object[] newArr = new Object[elements.length - 1];
+        for (int i = 0; i < elements.length; i++) {
+            if (index > 0) {
+                newArr[i] = elements[i];
+            } else {
+                newArr[i] = elements[i + 1];
+            }
+        }
+        return (E) newArr;
     }
-    
+    public int size(){
+        size = elements.length;
+        return size;
+    }
 }
